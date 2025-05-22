2a0c45cc75e4c9856b06d4a436e050c95d383e8f
Arne Mulder <info@label20.nl>
2025-05-22 20:10:59 +0200

    Add files via upload

---
+++ label20-cta-buttons.php
@@ -0,0 +1,251 @@
+<?php
+/**
+ * Plugin Name: Label 20 CTA Buttons
+ * Description: Zwevende contactknoppen met automatisch gegenereerde links en instelbare icoontjes.
+ * Version: 1.1.0
+ * Author: Arne Mulder
+ */
+
+if (!defined('ABSPATH')) exit;
+
+// Scripts en styles
+function l20cta_enqueue_scripts() {
+    wp_enqueue_style('font-awesome', 'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.0/css/all.min.css');
+    wp_enqueue_style('l20cta-style', plugin_dir_url(__FILE__) . 'style.css');
+    wp_enqueue_script('l20cta-script', plugin_dir_url(__FILE__) . 'script.js', [], false, true);
+
+    // Opties ophalen
+    $phone_number = get_option('l20cta_phone_number');
+    $email_address = get_option('l20cta_email_address');
+    $whatsapp_number = get_option('l20cta_whatsapp_number');
+    $whatsapp_text = get_option('l20cta_whatsapp_text');
+    $form_url = get_option('l20cta_form_url');
+
+    // Links genereren
+    $phone_url = ($phone_number) ? 'tel:' . preg_replace('/\D+/', '', $phone_number) : '';
+    $email_url = ($email_address) ? 'mailto:' . sanitize_email($email_address) : '';
+    $whatsapp_url = '';
+    if ($whatsapp_number) {
+        $clean_number = preg_replace('/\D+/', '', $whatsapp_number);
+        $text = rawurlencode($whatsapp_text ?: '');
+        $whatsapp_url = "https://api.whatsapp.com/send/?phone={$clean_number}&text={$text}";
+    }
+
+    wp_localize_script('l20cta-script', 'l20ctaData', [
+        'phone_url' => $phone_url,
+        'email_url' => $email_url,
+        'whatsapp_url' => $whatsapp_url,
+        'form_url' => esc_url($form_url),
+        'phone_icon_color' => get_option('l20cta_phone_icon_color'),
+        'email_icon_color' => get_option('l20cta_email_icon_color'),
+        'whatsapp_icon_color' => get_option('l20cta_whatsapp_icon_color'),
+        'form_icon_color' => get_option('l20cta_form_icon_color'),
+        'phone_bg_color' => get_option('l20cta_phone_bg_color'),
+        'email_bg_color' => get_option('l20cta_email_bg_color'),
+        'whatsapp_bg_color' => get_option('l20cta_whatsapp_bg_color'),
+        'form_bg_color' => get_option('l20cta_form_bg_color'),
+        'phone_order' => get_option('l20cta_phone_order'),
+        'email_order' => get_option('l20cta_email_order'),
+        'whatsapp_order' => get_option('l20cta_whatsapp_order'),
+        'form_order' => get_option('l20cta_form_order'),
+        'main_bg_color' => get_option('l20cta_main_bg_color'),
+        'main_icon_color' => get_option('l20cta_main_icon_color'),
+		'form_target_blank' => get_option('l20cta_form_target_blank', false),
+        'openings' => [
+            'maandag' => get_option('l20cta_opening_maandag'),
+            'dinsdag' => get_option('l20cta_opening_dinsdag'),
+            'woensdag' => get_option('l20cta_opening_woensdag'),
+            'donderdag' => get_option('l20cta_opening_donderdag'),
+            'vrijdag' => get_option('l20cta_opening_vrijdag'),
+            'zaterdag' => get_option('l20cta_opening_zaterdag'),
+            'zondag' => get_option('l20cta_opening_zondag'),
+        ]
+    ]);
+}
+add_action('wp_enqueue_scripts', 'l20cta_enqueue_scripts');
+
+// HTML output
+function l20cta_display_buttons() {
+    echo '<div class="l20cta-wrapper">
+    <div class="l20cta-buttons"></div>
+    <button id="l20cta-main-btn"><i class="fa fa-plus"></i></button>
+</div>';
+}
+add_action('wp_footer', 'l20cta_display_buttons');
+
+// Admin menu
+function l20cta_register_settings_page() {
+    add_options_page('Label 20 CTA Buttons', 'Label 20 CTA Buttons', 'manage_options', 'label-20-cta-buttons', 'l20cta_settings_page');
+}
+add_action('admin_menu', 'l20cta_register_settings_page');
+
+// Admin pagina
+function l20cta_settings_page() {
+    ?>
+    <div class="wrap">
+        <h1>Label 20 CTA Buttons Instellingen</h1>
+        <form method="post" action="options.php">
+            <?php
+            settings_fields('l20cta_settings_group');
+            do_settings_sections('l20cta_settings_group');
+            ?>
+
+            <h2>Algemeen</h2>
+            <table class="form-table">
+                <tr>
+                    <th>Achtergrondkleur hoofdknop</th>
+                    <td><input type="color" name="l20cta_main_bg_color" value="<?php echo esc_attr(get_option('l20cta_main_bg_color')); ?>"></td>
+                </tr>
+                <tr>
+                    <th>Kleur plus-icoon</th>
+                    <td><input type="color" name="l20cta_main_icon_color" value="<?php echo esc_attr(get_option('l20cta_main_icon_color')); ?>"></td>
+                </tr>
+            </table>
+
+            <h2>Buttons</h2>
+
+            <h3>Telefoon</h3>
+            <table class="form-table">
+                <tr>
+                    <th>Telefoonnummer (bijv. 0031612345678)</th>
+                    <td><input type="text" name="l20cta_phone_number" value="<?php echo esc_attr(get_option('l20cta_phone_number')); ?>"></td>
+                </tr>
+                <tr>
+                    <th>Volgorde</th>
+                    <td><input type="number" name="l20cta_phone_order" value="<?php echo esc_attr(get_option('l20cta_phone_order')); ?>"></td>
+                </tr>
+                <tr>
+                    <th>Achtergrondkleur</th>
+                    <td><input type="color" name="l20cta_phone_bg_color" value="<?php echo esc_attr(get_option('l20cta_phone_bg_color')); ?>"></td>
+                </tr>
+                <tr>
+                    <th>Kleur icoon</th>
+                    <td><input type="color" name="l20cta_phone_icon_color" value="<?php echo esc_attr(get_option('l20cta_phone_icon_color')); ?>"></td>
+                </tr>
+            </table>
+
+            <h3>E-mail</h3>
+            <table class="form-table">
+                <tr>
+                    <th>E-mailadres</th>
+                    <td><input type="email" name="l20cta_email_address" value="<?php echo esc_attr(get_option('l20cta_email_address')); ?>"></td>
+                </tr>
+                <tr>
+                    <th>Volgorde</th>
+                    <td><input type="number" name="l20cta_email_order" value="<?php echo esc_attr(get_option('l20cta_email_order')); ?>"></td>
+                </tr>
+                <tr>
+                    <th>Achtergrondkleur</th>
+                    <td><input type="color" name="l20cta_email_bg_color" value="<?php echo esc_attr(get_option('l20cta_email_bg_color')); ?>"></td>
+                </tr>
+                <tr>
+                    <th>Kleur icoon</th>
+                    <td><input type="color" name="l20cta_email_icon_color" value="<?php echo esc_attr(get_option('l20cta_email_icon_color')); ?>"></td>
+                </tr>
+            </table>
+
+            <h3>WhatsApp</h3>
+            <table class="form-table">
+                <tr>
+                    <th>WhatsApp telefoonnummer (bijv. 0031612345678)</th>
+                    <td><input type="text" name="l20cta_whatsapp_number" value="<?php echo esc_attr(get_option('l20cta_whatsapp_number')); ?>"></td>
+                </tr>
+                <tr>
+                    <th>WhatsApp standaardtekst</th>
+                    <td><input type="text" name="l20cta_whatsapp_text" value="<?php echo esc_attr(get_option('l20cta_whatsapp_text')); ?>"></td>
+                </tr>
+                <tr>
+                    <th>Volgorde</th>
+                    <td><input type="number" name="l20cta_whatsapp_order" value="<?php echo esc_attr(get_option('l20cta_whatsapp_order')); ?>"></td>
+                </tr>
+                <tr>
+                    <th>Achtergrondkleur</th>
+                    <td><input type="color" name="l20cta_whatsapp_bg_color" value="<?php echo esc_attr(get_option('l20cta_whatsapp_bg_color')); ?>"></td>
+                </tr>
+                <tr>
+                    <th>Kleur icoon</th>
+                    <td><input type="color" name="l20cta_whatsapp_icon_color" value="<?php echo esc_attr(get_option('l20cta_whatsapp_icon_color')); ?>"></td>
+                </tr>
+            </table>
+
+            <h3>Formulier</h3>
+            <table class="form-table">
+                <tr>
+                    <th>Formulier URL</th>
+                    <td><input type="url" name="l20cta_form_url" value="<?php echo esc_attr(get_option('l20cta_form_url')); ?>"></td>
+                </tr>
+                <tr>
+                    <th>Volgorde</th>
+                    <td><input type="number" name="l20cta_form_order" value="<?php echo esc_attr(get_option('l20cta_form_order')); ?>"></td>
+                </tr>
+                <tr>
+                    <th>Achtergrondkleur</th>
+                    <td><input type="color" name="l20cta_form_bg_color" value="<?php echo esc_attr(get_option('l20cta_form_bg_color')); ?>"></td>
+                </tr>
+                <tr>
+                    <th>Kleur icoon</th>
+                    <td><input type="color" name="l20cta_form_icon_color" value="<?php echo esc_attr(get_option('l20cta_form_icon_color')); ?>"></td>
+                </tr>
+				<tr>
+    <th>Open formulier in nieuw venster?</th>
+    <td>
+        <input type="checkbox" name="l20cta_form_target_blank" value="1" <?php checked(1, get_option('l20cta_form_target_blank'), true); ?>>
+        Ja
+    </td>
+</tr>
+            <h2>Openingstijden telefoon</h2>
+<p>Gebruik formaat <code>09:00-17:00</code>. Laat leeg als je gesloten bent.</p>
+<table class="form-table">
+<?php
+$days = ['maandag','dinsdag','woensdag','donderdag','vrijdag','zaterdag','zondag'];
+foreach ($days as $day) {
+    $key = strtolower($day);
+    ?>
+    <tr>
+        <th><?php echo ucfirst($day); ?></th>
+        <td>
+            <input type="text" name="l20cta_opening_<?php echo $key; ?>" value="<?php echo esc_attr(get_option("l20cta_opening_$key")); ?>" placeholder="09:00-17:00">
+        </td>
+    </tr>
+<?php } ?>
+</table>
+
+            <?php submit_button(); ?>
+        </form>
+    </div>
+    <?php
+}
+
+// Register settings
+function l20cta_register_settings() {
+    register_setting('l20cta_settings_group', 'l20cta_phone_number');
+    register_setting('l20cta_settings_group', 'l20cta_email_address');
+    register_setting('l20cta_settings_group', 'l20cta_whatsapp_number');
+    register_setting('l20cta_settings_group', 'l20cta_whatsapp_text');
+    register_setting('l20cta_settings_group', 'l20cta_form_url');
+	register_setting('l20cta_settings_group', 'l20cta_form_target_blank');
+
+
+    register_setting('l20cta_settings_group', 'l20cta_phone_icon_color');
+    register_setting('l20cta_settings_group', 'l20cta_email_icon_color');
+    register_setting('l20cta_settings_group', 'l20cta_whatsapp_icon_color');
+    register_setting('l20cta_settings_group', 'l20cta_form_icon_color');
+
+    register_setting('l20cta_settings_group', 'l20cta_phone_bg_color');
+    register_setting('l20cta_settings_group', 'l20cta_email_bg_color');
+    register_setting('l20cta_settings_group', 'l20cta_whatsapp_bg_color');
+    register_setting('l20cta_settings_group', 'l20cta_form_bg_color');
+
+    register_setting('l20cta_settings_group', 'l20cta_phone_order');
+    register_setting('l20cta_settings_group', 'l20cta_email_order');
+    register_setting('l20cta_settings_group', 'l20cta_whatsapp_order');
+    register_setting('l20cta_settings_group', 'l20cta_form_order');
+	$dagen = ['maandag','dinsdag','woensdag','donderdag','vrijdag','zaterdag','zondag'];
+foreach ($dagen as $dag) {
+    register_setting('l20cta_settings_group', "l20cta_opening_" . strtolower($dag));
+}
+
+    register_setting('l20cta_settings_group', 'l20cta_main_bg_color');
+    register_setting('l20cta_settings_group', 'l20cta_main_icon_color');
+}
+add_action('admin_init', 'l20cta_register_settings');
---
+++ script.js
@@ -0,0 +1,74 @@
+document.addEventListener('DOMContentLoaded', function () {
+    const mainBtn = document.getElementById('l20cta-main-btn');
+    const btnContainer = document.querySelector('.l20cta-buttons');
+
+    let buttons = [];
+
+    // Buttongegevens laden uit gelokaliseerde data
+    const data = l20ctaData;
+    const days = ['zondag','maandag','dinsdag','woensdag','donderdag','vrijdag','zaterdag'];
+    const now = new Date();
+    const today = days[now.getDay()];
+    const currentTime = now.getHours() * 60 + now.getMinutes();
+
+    const opening = data.openings[today];
+    let isPhoneOpen = false;
+
+    if (opening && opening.includes('-')) {
+        const [start, end] = opening.split('-');
+        const [startH, startM] = start.split(':').map(Number);
+        const [endH, endM] = end.split(':').map(Number);
+        const startMin = startH * 60 + startM;
+        const endMin = endH * 60 + endM;
+
+        isPhoneOpen = currentTime >= startMin && currentTime <= endMin;
+    }
+
+    const rawButtons = [
+        { type: 'phone', url: data.phone_url, icon: 'fa-phone', iconColor: data.phone_icon_color, bgColor: data.phone_bg_color, order: parseInt(data.phone_order), show: isPhoneOpen },
+        { type: 'email', url: data.email_url, icon: 'fa-envelope', iconColor: data.email_icon_color, bgColor: data.email_bg_color, order: parseInt(data.email_order), show: !!data.email_url },
+        { type: 'whatsapp', url: data.whatsapp_url, icon: 'fa-brands fa-whatsapp', iconColor: data.whatsapp_icon_color, bgColor: data.whatsapp_bg_color, order: parseInt(data.whatsapp_order), show: !!data.whatsapp_url },
+        { type: 'form', url: data.form_url, icon: 'fa-pen-to-square', iconColor: data.form_icon_color, bgColor: data.form_bg_color, order: parseInt(data.form_order), show: !!data.form_url, targetBlank: data.form_target_blank },
+    ];
+
+    // Filter en sorteer
+    buttons = rawButtons.filter(btn => btn.url && btn.show).sort((a, b) => a.order - b.order);
+
+    buttons.forEach(btn => {
+        const button = document.createElement('a');
+        button.href = btn.url;
+        button.className = 'l20cta-btn';
+        button.style.backgroundColor = btn.bgColor;
+        button.style.color = btn.iconColor;
+        button.innerHTML = `<i class="fa ${btn.icon}"></i>`;
+
+        if (btn.type === 'form') {
+            button.target = btn.targetBlank ? '_blank' : '_self';
+        } else {
+            button.target = '_blank';
+        }
+        btnContainer.appendChild(button);
+    });
+
+    // Hoofdknop kleuren instellen
+    mainBtn.style.backgroundColor = data.main_bg_color;
+    mainBtn.querySelector('i').style.color = data.main_icon_color;
+
+    // Toggle zichtbaar bij klik op hoofdknop
+    mainBtn.addEventListener('click', function (event) {
+        event.stopPropagation(); // voorkom sluiten direct na openen
+        btnContainer.classList.toggle('l20cta-visible');
+        mainBtn.classList.toggle('l20cta-open');
+    });
+
+    // Sluit menu bij klik buiten de knoppen
+    document.addEventListener('click', function (event) {
+        const clickedInsideMain = mainBtn.contains(event.target);
+        const clickedInsideButtons = btnContainer.contains(event.target);
+
+        if (!clickedInsideMain && !clickedInsideButtons) {
+            btnContainer.classList.remove('l20cta-visible');
+            mainBtn.classList.remove('l20cta-open');
+        }
+    });
+});
